c052c3f31ac9717799704419bc28a74790268e28
Liam Asman <Liam@LiamAsman.com>
2025-04-29 18:00:17 +0100

    Add Oh-My-Zsh installation to script

--- setup.sh
+++ setup.sh
@@ -1,4 +1,11 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "$BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
 git config --global init.defaultBranch main
 git config --global user.name "Liam Asman"
 git config --global user.email "Liam@LiamAsman.com"
 sudo apt install zsh -y
+sudo apt install curl -y
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+ln -s "${SCRIPT_DIR}.zshrc" ~/.zshrc
