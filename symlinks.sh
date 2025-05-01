8fb6e8553aec386d2aa85e8f62b4ff2f54cf3232
Liam Asman <liamma96@gmail.com>
2025-05-01 09:05:22 +0100

    Fix symlink for nvm config

--- symlinks.sh
+++ symlinks.sh
@@ -7,5 +7,5 @@ ln -sf "${SCRIPT_DIR}/.tmux.conf" ~/.tmux.conf
 ln -sf "${SCRIPT_DIR}/.zshrc" ~/.zshrc
 ln -sf "${SCRIPT_DIR}/.aliases" ~/.aliases
 mkdir -p ~/.config
-ln -sf "${SCRIPT_DIR}/nvm" ~/.config/nvim
+ln -sf "${SCRIPT_DIR}/.config/nvm" ~/.config/nvim
 ln -sf "${SCRIPT_DIR}/starship.toml" ~/.config/starship.toml
