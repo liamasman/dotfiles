00f383a9dfb964f12f2af017615c857ad7feca9c
Liam Asman <liamma96@gmail.com>
2025-05-01 08:50:28 +0100

    Add aliases to symlinks

--- symlinks.sh
+++ symlinks.sh
@@ -5,6 +5,7 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "$BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 ln -sf "${SCRIPT_DIR}/.bashrc" ~/.bashrc
 ln -sf "${SCRIPT_DIR}/.tmux.conf" ~/.tmux.conf
 ln -sf "${SCRIPT_DIR}/.zshrc" ~/.zshrc
+ln -sf "${SCRIPT_DIR}/.aliases" ~/.aliases
 mkdir -p ~/.config
 ln -sf "${SCRIPT_DIR}/nvm" ~/.config/nvim
 ln -sf "${SCRIPT_DIR}/starship.toml" ~/.config/starship.toml
