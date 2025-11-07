3ea2d0d0d149ab77883038fb9d813a0ce1ad1e68
Liam Asman <Liam@LiamAsman.com>
2025-11-07 16:03:22 +0000

    Fix symlinks.sh

--- symlinks.sh
+++ symlinks.sh
@@ -2,11 +2,11 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "$BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-ln -sf "${SCRIPT_DIR}/.bashrc" ~/.bashrc
-ln -sf "${SCRIPT_DIR}/.tmux.conf" ~/.tmux.conf
-ln -sf "${SCRIPT_DIR}/.zshrc" ~/.zshrc
-ln -sf "${SCRIPT_DIR}/.aliases" ~/.aliases
-mkdir -p ~/.config
-ln -sf "${SCRIPT_DIR}/.config/nvim" ~/.config/nvim
-ln -sf "${SCRIPT_DIR}/.config/starship.toml" ~/.config/starship.toml
-ln -sf "${SCRIPT_DIR}/.tip_of_the_day.sh" ~/.tip_of_the_day.sh
+ln -s -f "${SCRIPT_DIR}/.bashrc" ${HOME}/.bashrc
+ln -s -f "${SCRIPT_DIR}/.tmux.conf" ${HOME}/.tmux.conf
+ln -s -f "${SCRIPT_DIR}/.zshrc" ${HOME}/.zshrc
+ln -s -f "${SCRIPT_DIR}/.aliases" ${HOME}/.aliases
+mkdir -p ${HOME}/.config
+ln -s -f "${SCRIPT_DIR}/.config/nvim" ${HOME}/.config/nvim
+ln -s -f "${SCRIPT_DIR}/.config/starship.toml" ${HOME}/.config/starship.toml
+ln -s -f "${SCRIPT_DIR}/.tip_of_the_day.sh" ${HOME}/.tip_of_the_day.sh
